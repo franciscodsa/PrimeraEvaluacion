ee48646eae3ae7e80d114b87dcea41a4cab722df
anaijg <ana.jimenezgomez@educa.madrid.org>
2022-10-18 21:15:59 +0200

    Ejercicios for

---
+++ src/estructurasdecontrol/Ejercicio24.java
@@ -0,0 +1,26 @@
+package estructurasdecontrol;
+
+import java.util.Scanner;
+
+public class Ejercicio24 {
+    public static void main(String[] args) {
+//        24. Hacer un programa que pida dos números a y b, enteros, por teclado (se asume que a es
+//                menor que b siempre), e imprima la suma de todos los números enteros desde a hasta b,
+//        ambos inclusive.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("a: ");
+        int a = sc.nextInt();
+        System.out.print("b: ");
+        int b = sc.nextInt();
+
+        // tenemos que hacer una suma desde a hasta b
+        // declaramos una variable suma inicializada a 0
+        int suma = 0;
+        for (int i = a; i <= b; i++) {
+            // vamos sumando a suma todos los números desde a hasta b
+            suma = suma + i;
+        }
+        System.out.println("Resultado: " + suma);
+
+    }
+}
